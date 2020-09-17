9aaa4e855e3494fdbc9983e109d4982037e00281
maddi1337 <mabrass@web.de>
2020-09-17 15:41:38 +0200

    deactivated zipping

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+git pull
 wget --output-file="logs.csv" "https://docs.google.com/spreadsheets/d/1nzvrh5GzZoReQNvHqeA0ExIgfYsCS7437XNpexbIGiQ/export?format=tsv&gid=356540164" -O "2content.tsv"
 cat *.tsv > loot_table.lua
 cp loot_table.lua ./HIP_Loot_Priority/
-zip -r HIP_Loot_Priority.zip ./HIP_Loot_Priority/
-cp HIP_Loot_Priority ../hip-loot -r
+#zip -r HIP_Loot_Priority.zip ./HIP_Loot_Priority/
+cp HIP_Loot_Priority/* ../hip-loot -r
