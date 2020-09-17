95c1888d45e9f58a169df89d0ea8da3d4a312494
groeni1337 <71442150+groeni1337@users.noreply.github.com>
2020-09-17 13:59:55 +0200

    Update ver.lua

--- HIP_Loot_Priority/ver.lua
+++ HIP_Loot_Priority/ver.lua
@@ -6,9 +6,9 @@ f:SetScript("OnEvent", function(f, event)
     end
 end)
 
-SLASH_TBO1 = "/tbo"
-SLASH_TBO2 = "/tbo2"
-SlashCmdList["TBO"] = function(msg)
+SLASH_HIP1 = "/hip"
+SLASH_HIP2 = "/hip2"
+SlashCmdList["HIP"] = function(msg)
    print("HiP Loot Priority 1.0.1 - Stand 16.09.2020")
 end 
 
