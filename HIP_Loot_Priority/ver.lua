72518f725cbf66f862049501b2b70cf58d7b6b82
groeni1337 <71442150+groeni1337@users.noreply.github.com>
2020-09-23 10:01:59 +0200

    Update ver.lua

--- HIP_Loot_Priority/ver.lua
+++ HIP_Loot_Priority/ver.lua
@@ -2,13 +2,13 @@ local f = CreateFrame("Frame")
 f:RegisterEvent("PLAYER_LOGIN")
 f:SetScript("OnEvent", function(f, event)
     if event == "PLAYER_LOGIN" then
-      print("HiP Loot Priority 1.0.1 - Stand 16.09.2020")
+      print("HiP Loot Priority 1.0.1 - Phase 5")
     end
 end)
 
 SLASH_HIP1 = "/hip"
 SLASH_HIP2 = "/hip2"
 SlashCmdList["HIP"] = function(msg)
-   print("HiP Loot Priority 1.0.1 - Stand 16.09.2020")
+   print("HiP Loot Priority 1.0.1 - Phase 5")
 end 
 
