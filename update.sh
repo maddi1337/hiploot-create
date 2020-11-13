6702d8265d9aa01f1cb41acf51ee560b9021672a
maddi1337 <>
2020-11-13 13:05:50 +0100

    pull befor push

--- update.sh
+++ update.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 git pull
+cd ../hip-loot
+git pull
+cd ../hip-loot-create
 wget --output-file="logs.csv" "https://docs.google.com/spreadsheets/d/1nzvrh5GzZoReQNvHqeA0ExIgfYsCS7437XNpexbIGiQ/export?format=tsv&gid=356540164" -O "2content.tsv"
 cat *.tsv > loot_table.lua
 cp loot_table.lua ./HIP_Loot_Priority/
